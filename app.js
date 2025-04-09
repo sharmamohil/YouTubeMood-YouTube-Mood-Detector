50bb961d93d1e7aeb18386d4f19cbbfc2f8f2fe4
Mohil Sharma <158797105+sharmamohil@users.noreply.github.com>
2025-04-09 10:35:59 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+// App.js
+import React from 'react';
+import Navbar from './Navbar';
+import Home from './Home';
+import './styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
